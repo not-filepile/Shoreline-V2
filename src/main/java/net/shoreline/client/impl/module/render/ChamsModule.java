f651c1cf3f99bfc8642eb3460583eb3902d7f6c2
not-filepile <pjwearth1@gmail.com>
2026-04-01 23:59:39 +0900

    -chams crash

--- src/main/java/net/shoreline/client/impl/module/render/ChamsModule.java
+++ src/main/java/net/shoreline/client/impl/module/render/ChamsModule.java
@@ -129,6 +129,11 @@ public class ChamsModule extends RenderModule
 
     public boolean isValid(Entity entity)
     {
+        if (entity instanceof FakePlayerEntity)
+        {
+            return false;
+        }
+
         if (!Managers.RENDER.isVisible(entity.getBoundingBox())
                 || MathHelper.square(range.getValue()) < entity.squaredDistanceTo(getCameraPos()))
         {
--- src/main/java/net/shoreline/client/mixin/render/entity/MixinEndCrystalEntityRenderer.java
+++ src/main/java/net/shoreline/client/mixin/render/entity/MixinEndCrystalEntityRenderer.java
@@ -93,29 +93,7 @@ public class MixinEndCrystalEntityRenderer
                     shift = At.Shift.AFTER))
     private void setAnglesHook(EndCrystalEntityRenderState endCrystalEntityRenderState, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo info)
     {
-        if (ChamsRenderer.rendering)
-        {
-            return;
-        }
-
-        boolean valid = ChamsModule.getInstance().isValid(last);
-        ((IModel) model).cancelModel(valid);
-        if (ChamsModule.getInstance().isEnabled() && valid)
-        {
-            int color = ChamsModule.getInstance().getColor(last).getRGB();
-            if (ChamsModule.getInstance().mode.getValue() == ChamsModule.ChamsMode.SHINE)
-            {
-                Layers.QUADS_GLINT.startDrawing();
-                VertexConsumerProvider provider = MinecraftClient.getInstance().getBufferBuilders().getEffectVertexConsumers();
-                VertexConsumer consumer = ItemRenderer.getArmorGlintConsumer(provider, Layers.QUADS_GLINT, true);
-                model.render(matrixStack, consumer, i, OverlayTexture.DEFAULT_UV, color);
-                Layers.QUADS_GLINT.endDrawing();
-            }
-
-            ChamsRenderer renderer = ChamsModule.getInstance().mode.getValue().getRenderer();
-            float tickDelta = MinecraftClient.getInstance().getRenderTickCounter().getTickDelta(false);
-            ChamsRenderer.render(renderer, last, tickDelta, color);
-        }
+        // Disabled
     }
 
     @Redirect(
